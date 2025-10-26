2cdb0b364cab34b8010b9639e9826515e02d1b45
Neel Shah <neelalpesh1710@gmail.com>
2025-10-26 12:02:54 -0400

    initial commit for setting up the repo

---
+++ http-server/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout <<"Hell0, this is the beginning of the project http server written in c++ by Neel Shah";
+    std::cout <<std::endl; //Insert a new line and flush the buffer
+
+    return 0;
+    
+}
